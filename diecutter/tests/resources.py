8ad0bbd6ae117c6f4e2e35abd2e8c2b93dcd82c5
Benoît Bryon <benoit@marmelune.net>
2013-02-11 10:35:06 +0100

    More tests around FileResource.

--- diecutter/tests/resources.py
+++ diecutter/tests/resources.py
@@ -5,17 +5,21 @@ from os.path import exists, isdir, isfile, join
 import unittest
 
 from diecutter import resources
+from diecutter.exceptions import TemplateError
 from diecutter.tests import temporary_directory
 
 
 class MockEngine(object):
     """Template engine mock."""
-    def __init__(self, render_result=u'this is a mock'):
+    def __init__(self, render_result=u'this is a mock', fail=None):
         self.render_result = render_result
+        self.fail = fail
 
-    def render(self, template, context):
-        """Return :py:attr:`render_result`."""
-        return self.render_result
+    def render(self, *args, **kwargs):
+        """Return tuple (:py:attr:`render_result`, ``args``, ``kwargs``)."""
+        if self.fail:
+            raise TemplateError(self.fail)
+        return (self.render_result, args, kwargs)
 
 
 class FileResourceTestCase(unittest.TestCase):
@@ -49,3 +53,45 @@ class FileResourceTestCase(unittest.TestCase):
             self.assertTrue(isfile(path))  # Check initial status.
             resource = resources.FileResource(path=path, engine=None)
             self.assertTrue(resource.exists is True)
+
+    def test_read_empty(self):
+        """FileResource.read() empty file returns empty string."""
+        with temporary_directory() as template_dir:
+            path = join(template_dir, 'dummy')
+            open(path, 'w')
+            resource = resources.FileResource(path=path, engine=None)
+            self.assertEqual(resource.read(), u'')
+
+    def test_read_utf8(self):
+        """FileResource.read() decodes UTF-8 files."""
+        with temporary_directory() as template_dir:
+            path = join(template_dir, 'dummy')
+            content = u'Thé ou café ?'
+            open(path, 'w').write(content.encode('utf8'))
+            resource = resources.FileResource(path=path, engine=None)
+            self.assertEqual(resource.read(), content)
+
+    def test_render(self):
+        """FileResource.render() renders template against context."""
+        with temporary_directory() as template_dir:
+            path = join(template_dir, 'dummy')
+            template = u'Tea or coffee'
+            context = {'a': 1, 'b': 2}
+            engine = MockEngine(u'this is render result')
+            open(path, 'w').write(template.encode('utf8'))
+            resource = resources.FileResource(path=path, engine=engine)
+            result = resource.render(context)
+            self.assertEqual(result, (u'this is render result',  # Mock result.
+                                      (template, context),  # args.
+                                      {}))  # kwargs.
+
+    def test_render_error(self):
+        """FileResource.render() raises ``TemplateError`` in case of fail."""
+        with temporary_directory() as template_dir:
+            path = join(template_dir, 'dummy')
+            template = u'Tea or coffee'
+            context = {'a': 1, 'b': 2}
+            engine = MockEngine(fail=u'This is an error message')
+            open(path, 'w').write(template.encode('utf8'))
+            resource = resources.FileResource(path=path, engine=engine)
+            self.assertRaises(TemplateError, resource.render, context)
