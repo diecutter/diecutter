4bed3678e6e47b1fbeff0723fdc976528201aa06
Benoît Bryon <benoit@marmelune.net>
2013-03-28 09:12:02 +0100

    Refs #37 - Used jinja.Environment in diecutter.engines.Jinja2Engine. Introduced tests for Jinja2Engine.

--- diecutter/engines/jinja.py
+++ diecutter/engines/jinja.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Jinja2 template engine."""
-from jinja2 import Template
+from jinja2 import Environment
 from jinja2.exceptions import UndefinedError, TemplateSyntaxError
 
 from diecutter.engines import Engine
@@ -9,10 +9,15 @@ from diecutter.exceptions import TemplateError
 
 class Jinja2Engine(Engine):
     """Jinja2 template engine."""
+    def __init__(self, environment=None):
+        if environment is None:
+            environment = Environment()
+        self.environment = environment
+
     def render(self, template, context):
         """Return the rendered template against context."""
         try:
-            template = Template(template)
+            template = self.environment.from_string(template)
         except TemplateSyntaxError as e:
             raise TemplateError(e)
         try:
---
+++ diecutter/tests/engines/jinja.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""Tests around diecutter.engines.jinja."""
+import unittest
+
+from diecutter.engines.jinja import Jinja2Engine
+from diecutter.exceptions import TemplateError
+
+
+class Jinja2TestCase(unittest.TestCase):
+    """Test diecutter.engines.jinja.Jinja2Engine."""
+    def test_render_noop(self):
+        """Jinja2Engine correctly renders ``Hello world!`` template."""
+        engine = Jinja2Engine()
+        rendered = engine.render(u'Hello world!', {})
+        self.assertEqual(rendered, u'Hello world!')
+
+    def test_render_simple(self):
+        """Jinja2Engine correctly renders ``Hello {{ name }}!`` template."""
+        engine = Jinja2Engine()
+        rendered = engine.render(u'Hello {{ name }}!', {'name': 'world'})
+        self.assertEqual(rendered, u'Hello world!')
+
+    def test_template_error(self):
+        """Jinja2Engine raises TemplateError in case of exception."""
+        engine = Jinja2Engine()
+        self.assertRaises(TemplateError,
+                          engine.render,
+                          u'{% if foo %}Unclosed IF',
+                          {})
