d9eb9fc0b64455e3e7377dbcc0b1ceda28fe2513
Benoît Bryon <benoit@marmelune.net>
2013-04-05 09:15:46 +0200

    Improved coverage by adding doctest to diecutter.engines.Engine.

--- diecutter/engines/__init__.py
+++ diecutter/engines/__init__.py
@@ -7,7 +7,17 @@ class Engine(object):
 
     Mostly used to document engine API.
 
+    Subclasses must implement :py:meth:`render`:
+
+    >>> from diecutter.engines import Engine
+    >>> engine = Engine()
+    >>> engine.render('fake-template', {'fake': 1})  # Doctest: +ELLIPSIS
+    Traceback (most recent call last):
+      ...
+    NotImplementedError: Subclasses of "diecutter.engines.Engine" must implement render() method.
+
     """
     def render(self, template, context):
         """Return the rendered template against context."""
-        raise NotImplementedError()
+        raise NotImplementedError('Subclasses of "diecutter.engines.Engine" '
+                                  'must implement render() method.')
