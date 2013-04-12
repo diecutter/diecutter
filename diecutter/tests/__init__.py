dcd1e7bbec63794fb39bae5f72d3ce3132e340e4
Benoît Bryon <benoit@marmelune.net>
2013-04-12 10:00:32 +0200

    Refs #23, refs #25 - Using webtest's StopableWSGIServer to run doctests with test server.

--- diecutter/tests/__init__.py
+++ diecutter/tests/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """Tests."""
+import os
 import shutil
 import tempfile
+
+from webtest.http import StopableWSGIServer
+
+import diecutter
 
 
 class temporary_directory(object):
@@ -34,3 +39,36 @@ class temporary_directory(object):
     def __exit__(self, exc_type=None, exc_val=None, exc_tb=None):
         """Remove temporary directory recursively."""
         shutil.rmtree(self.path)
+
+
+def demo_template_dir():
+    """Return absolute path to diecutter's demo template dir."""
+    tests_dir = os.path.dirname(os.path.abspath(__file__))
+    package_dir = os.path.dirname(tests_dir)
+    project_dir = os.path.dirname(package_dir)
+    demo_dir = os.path.join(project_dir, 'demo')
+    return os.path.normpath(os.path.join(demo_dir, 'templates'))
+
+
+def settings(template_dir):
+    """Shortcut to get diecutter settings for use in WSGI application."""
+    return {'diecutter.template_dir': template_dir}
+
+
+def wsgi_application(settings={}):
+    """Return diecutter WSGI application for tests.
+
+    Uses WebTest.
+
+    """
+    global_config = {}
+    application = diecutter.main(global_config, **settings)
+    #application = TestApp(application)
+    return application
+
+
+def wsgi_server(application):
+    """Return (running) WebTest's StopableWSGIServer for application."""
+    server = StopableWSGIServer.create(application)
+    server.wait()
+    return server
