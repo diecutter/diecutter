5fd6f8d6899386d0c89b42d66dd6b3f880e47309
Benoît Bryon <benoit@marmelune.net>
2013-07-06 17:43:56 +0200

    Refs #27 - Refactored implementation of Github resources: cloning repository. WORK IN PROGRESS.

--- diecutter/github.py
+++ diecutter/github.py
@@ -1,10 +1,17 @@
-import base64
-import json
-
-from pyramid.exceptions import NotFound
-import requests
+import os
+import tempfile
 
 from diecutter.resources import FileResource, DirResource
+
+
+def execute(command):
+    import subprocess
+    process = subprocess.Popen(command,
+                               stdin=None,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               shell=False)
+    return (process.wait(), process.stdout.read(), process.stderr.read())
 
 
 class GithubResource(object):
@@ -22,160 +29,88 @@ class GithubResource(object):
         """
         return path.lstrip('/').split('/', 3)
 
-    def github_response(self, url):
-        """Retrieve and return response from Github API.
-
-        >>> from diecutter.github import GithubResource
-        >>> res = GithubResource()
-        >>> url = res.github_content_url(user='novagile',
-        ...                              project='diecutter',
-        ...                              commit='master',
-        ...                              path='/demo/templates')
-        >>> response = res.github_response(url)
-        >>> response.status_code
-        200
-
-        """
+    def github_checkout(self, user, project, commit, output_dir=None):
+        """Return path to local checkout of remote repository."""
         try:
-            response = requests.get(url)
-        except requests.exceptions.RequestException as e:
-            raise e
-        if response.status_code == 404:
-            raise NotFound(url)
-        return response
+            return self._checkout
+        except AttributeError:
+            if output_dir is None:
+                output_dir = tempfile.mkdtemp()
+            self.github_clone(user, project, output_dir)
+            command = ['git', 'checkout', commit]
+            previous_dir = os.getcwd()
+            os.chdir(output_dir)
+            try:
+                code, stdout, stderr = execute(command)
+            finally:
+                os.chdir(previous_dir)
+            self._checkout = output_dir
+            return self._checkout
 
-    def github_content_url(self, user, project, commit, path):
-        """Return URL of content on Github API.
+    def github_clone(self, user, project, output_dir):
+        """Clone repository locally over ssh in ``output_dir``."""
+        command = ['git', 'clone', '--no-checkout',
+                   self.github_clone_url(user, project),
+                   output_dir]
+        code, stdout, stderr = execute(command)
 
-        >>> from diecutter.github import GithubResource
-        >>> res = GithubResource()
-        >>> res.github_content_url('novagile', 'diecutter', 'master', '/demo')
-        u'https://api.github.com/repos/novagile/diecutter/contents/demo?ref=master'
-
-        """
-        return u'https://api.github.com/repos/{user}/{project}/contents/' \
-               u'{path}?ref={commit}'.format(user=user,
-                                             project=project,
-                                             path=path.lstrip('/'),
-                                             commit=commit)
-
-    def github_tree_url(self, user, project, commit):
-        """Return URL of tree on Github API.
-
-        >>> from diecutter.github import GithubResource
-        >>> res = GithubResource()
-        >>> res.github_tree_url('novagile', 'diecutter', 'master')
-        u'https://api.github.com/repos/novagile/diecutter/git/trees/master?recursive=1'
-
-        """
-        return u'https://api.github.com/repos/{user}/{project}/git/trees/' \
-               u'{commit}?recursive=1'.format(user=user,
-                                              project=project,
-                                              commit=commit)
-
-    def is_file(self, path):
-        """Return True if resource is a file.
-
-        >>> from diecutter.github import GithubResource
-        >>> res = GithubResource()
-        >>> res.is_file('/novagile/diecutter/master/demo/templates')
-        False
-        >>> res.is_file('/novagile/diecutter/master/README')
-        True
-
-        """
-        user, project, commit, path = self.split_path(path)
-        url = self.github_content_url(user, project, commit, path)
-        response = self.github_response(url)
-        data = json.loads(response.text)
-        return 'content' in data
+    def github_clone_url(self, user, project):
+        """Return URL to clone from github."""
+#        return '/home/benoit/web/diecutter/'
+        return 'git@github.com:{user}/{project}.git'.format(user=user,
+                                                            project=project)
 
 
 class GithubFileResource(GithubResource, FileResource):
-    """A file resource that lives on Github."""
-    @property
-    def is_file(self):
-        return super(GithubResource, self).is_file
+    """A file resource that lives on Github.
 
-    def read(self):
-        """Return file contents.
+    >>> from diecutter.github import GithubFileResource
+    >>> from diecutter.tests import temporary_directory
+    >>> with temporary_directory() as working_dir:
+    ...     res = GithubFileResource(
+    ...         '/novagile/diecutter/967556784a99d593120718c222051d8b4766e176/demo/templates/greetings.txt',
+    ...         local_dir=working_dir)
+    ...     res.exists
+    ...     res.is_file
+    ...     print res.read()
+    True
+    True
+    {{ greetings|default('Hello') }} {{ name }}!
+    <BLANKLINE>
 
-        >>> from diecutter.github import GithubFileResource
-        >>> res = GithubFileResource('/novagile/diecutter/master/demo/'
-        ...                          'templates/greetings.txt')
-        >>> print res.read()
-        {{ greetings|default('Hello') }} {{ name }}!
-        <BLANKLINE>
-
-        """
-        user, project, commit, path = self.split_path(self.path)
-        url = self.github_content_url(user, project, commit, path)
-        response = self.github_response(url)
-        data = json.loads(response.text)
-        try:
-            content = data['content']
-        except KeyError:
-            raise Exception('Not a file')
-        if data['encoding'] == 'base64':
-            content = unicode(base64.b64decode(content))
-        else:
-            raise Exception('Unsupported encoding %s' % data['encoding'])
-        return content
+    """
+    def __init__(self, path='', engine=None, filename_engine=None,
+                 local_dir=None):
+        self.remote_path = path
+        user, project, commit, relative_path = self.split_path(path)
+        local_dir = GithubResource.github_checkout(
+            self, user, project, commit, local_dir)
+        local_path = os.path.join(local_dir, relative_path)
+        FileResource.__init__(self, local_path, engine, filename_engine)
 
 
 class GithubDirResource(GithubResource, DirResource):
-    """A directory resource that lives on Github."""
-    @property
-    def exists(self):
-        """Return True if resource exists.
+    """A directory resource that lives on Github.
 
-        >>> from diecutter.github import GithubDirResource
-        >>> res = GithubDirResource('/novagile/diecutter/master/demo/templates')
-        >>> res.exists
-        True
-        >>> res = GithubDirResource('/novagile/diecutter/master/fake')
-        >>> res.exists
-        False
+    >>> from diecutter.github import GithubDirResource
+    >>> from diecutter.tests import temporary_directory
+    >>> with temporary_directory() as working_dir:
+    ...     res = GithubDirResource('/novagile/diecutter/967556784a99d593120718c222051d8b4766e176/demo/templates/dynamic-tree/',
+    ...         local_dir=working_dir)
+    ...     res.exists
+    ...     res.is_dir
+    ...     print res.read()
+    True
+    True
+    .diecutter-tree
+    greeter.txt
 
-        """
-        user, project, commit, path = self.split_path(self.path)
-        url = self.github_content_url(user, project, commit, path)
-        try:
-            self.github_response(url)
-        except NotFound:
-            return False
-        return True
-
-    @property
-    def content_type(self):
-        return 'application/zip'
-
-    def relative_filename(self, filename):
-        """Return filename relative to :py:attr:`path`."""
-        if filename.startswith(self.path):
-            return filename[len(self.path):].lstrip('/')
-        return filename
-
-    def read_tree(self):
-        """Generate list of paths to contained resources.
-
-        >>> from diecutter.github import GithubDirResource
-        >>> res = GithubDirResource('/novagile/diecutter/master/demo/templates/'
-        ...                         'dynamic-tree')
-        >>> list(res.read_tree())
-        [u'/novagile/diecutter/master/demo/templates/dynamic-tree/.diecutter-tree', u'/novagile/diecutter/master/demo/templates/dynamic-tree/greeter.txt']
-
-        """
-        user, project, commit, path = self.split_path(self.path)
-        url = self.github_tree_url(user, project, commit)
-        response = self.github_response(url)
-        data = json.loads(response.text)
-        for item in data['tree']:
-            if item['type'] == u'blob':
-                if item['path'].startswith(path):
-                    yield u'/'.join([u'', user, project, commit, item['path']])
-
-    def get_file_resource(self, path):
-        """Factory for internal FileResources."""
-        file_path = u'/'.join(self.path.rstrip(u'/'), path.lstrip(u'/'))
-        return GithubFileResource(file_path, self.engine)
+    """
+    def __init__(self, path='', engine=None, filename_engine=None,
+                 local_dir=None):
+        self.remote_path = path
+        user, project, commit, relative_path = self.split_path(path)
+        local_dir = GithubResource.github_checkout(
+            self, user, project, commit, local_dir)
+        local_path = os.path.join(local_dir, relative_path)
+        DirResource.__init__(self, local_path, engine, filename_engine)
