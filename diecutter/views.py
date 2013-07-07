f9ffa3819ee7652da73c334c40a6f4cc2e5da1e7
Benoît Bryon <benoit@marmelune.net>
2013-07-07 17:50:59 +0200

    Refs #27 - Refactored implementation of diecutter service: preparing configurable services. WORK IN PROGRESS.

--- diecutter/views.py
+++ diecutter/views.py
@@ -9,8 +9,6 @@ from pyramid.httpexceptions import HTTPNotImplemented, HTTPNotAcceptable
 from webob.acceptparse import MIMENilAccept
 
 from diecutter import __version__ as VERSION
-from diecutter.engines.filename import FilenameEngine
-from diecutter.engines.jinja import Jinja2Engine
 from diecutter import resources
 from diecutter.contextextractors import extract_context
 from diecutter.validators import token_validator
@@ -22,7 +20,8 @@ logger = logging.getLogger(__name__)
 class Service(object):
     """Base class for diecutter services."""
     def hello(self, request):
-        raise NotImplementedError()
+        """Returns Hello and API version in JSON."""
+        return {'diecutter': 'Hello', 'version': VERSION}
 
     def get(self, request):
         raise NotImplementedError()
@@ -33,15 +32,67 @@ class Service(object):
     def put(self, request):
         raise NotImplementedError()
 
+    def get_resource(self, request):
+        """Return the resource object (instance) matching request."""
+        raise NotImplementedError()
+
+    def get_engine(self, request):
+        """Return configured template engine to render templates."""
+        from diecutter.engines.jinja import Jinja2Engine
+        return Jinja2Engine()
+
+    def get_filename_engine(self, request):
+        """Return configured template engine to render filenames.
+
+        This is not used for dynamic trees.
+
+        """
+        from diecutter.engines.filename import FilenameEngine
+        return FilenameEngine()
+
+    def get_writers(self, request, resource, context):
+        """Return iterable of writers."""
+        from diecutter.writers import (zip_directory_response, file_response,
+                                       targz_directory_response)
+        if resource.is_file:
+            return [file_response]
+        else:
+            accepted_mime_types = get_accepted_types(request)
+            # Reference.
+            mime_map = {'application/zip': [zip_directory_response],
+                        'application/gzip': [targz_directory_response],
+                        }
+            # Aliases.
+            mime_map['application/x-gzip'] = mime_map['application/gzip']
+            # Fallback.
+            mime_map['*/*'] = mime_map['application/zip']
+            for accepted_mime_type in accepted_mime_types:
+                try:
+                    return mime_map[accepted_mime_type]
+                except KeyError:
+                    pass
+            raise HTTPNotAcceptable('Supported mime types: %s'
+                                    % ', '.join(sorted(mime_map.keys())))
+
+    def get_dispatcher(self, request, resource, context, writers):
+        """Return simple dispatcher (later, would read configuration)."""
+        return FirstResultDispatcher(writers)
+
+    def is_readonly(self, request):
+        """Return "readonly" flag status (boolean) for request.
+
+        As an example, PUT operations should be forbidden if readonly flag is
+        On.
+
+        """
+        return to_boolean(request.registry.settings.get('diecutter.readonly',
+                                                        False))
+
 
 class LocalService(Service):
     """A service that loads templates on local filesystem."""
-    def hello(self, request):
-        """Returns Hello in JSON."""
-        return {'diecutter': 'Hello', 'version': VERSION}
-
     def get(self, request):
-        resource = get_resource(request)
+        resource = self.get_resource(request)
         if not resource.exists:
             return NotFound('Template not found')
         request.response.content_type = 'text/plain'
@@ -49,7 +100,7 @@ class LocalService(Service):
         return request.response
 
     def post(self, request):
-        resource = get_resource(request)
+        resource = self.get_resource(request)
         try:
             context = extract_context(request)
         except NotImplementedError as e:
@@ -61,18 +112,18 @@ class LocalService(Service):
             'now': datetime.now()}
         if not resource.exists:
             return NotFound('Template not found')
-        writers = get_writers(request, resource, context)
-        dispatcher = get_dispatcher(request, resource, context, writers)
+        writers = self.get_writers(request, resource, context)
+        dispatcher = self.get_dispatcher(request, resource, context, writers)
         response = dispatcher(request, resource, context)
         return response
 
     def put(self, request):
-        if is_readonly(request):
+        if self.is_readonly(request):
             raise Forbidden('This diecutter server is readonly.')
         filename = request.matchdict['template_path']
         input_file = request.POST['file'].file
 
-        file_path = get_resource_path(request)
+        file_path = self.get_resource_path(request)
 
         if not exists(dirname(file_path)):
             makedirs(dirname(file_path))
@@ -86,52 +137,49 @@ class LocalService(Service):
         request.response.headers['location'] = str('/%s' % filename)
         return {'diecutter': 'Ok'}
 
+    def get_resource(self, request):
+        """Return the resource matching request.
 
-def get_template_dir(request):
-    """Return validated template directory configuration for request."""
-    try:
-        template_dir = request.registry.settings['diecutter.template_dir']
-    except KeyError:
-        error_msg = 'Missing mandatory "diecutter.template_dir" setting.'
-        raise ConfigurationError(error_msg)
-    if not template_dir:
-        error_msg = 'Mandatory "diecutter.template_dir" setting is empty.'
-        raise ConfigurationError(error_msg)
-    return template_dir
+        Return value is a :py:class:`FileResource` or :py:class`DirResource`.
 
+        """
+        path = self.get_resource_path(request)
+        engine = self.get_engine(request)
+        filename_engine = self.get_filename_engine(request)
+        if isdir(path):
+            resource = resources.DirResource(path=path, engine=engine,
+                                             filename_engine=filename_engine)
+        else:
+            resource = resources.FileResource(path=path, engine=engine,
+                                              filename_engine=filename_engine)
+        return resource
 
-def get_resource_path(request):
-    """Return validated (absolute) resource path from request.
+    def get_resource_path(self, request):
+        """Return validated (absolute) resource path from request.
 
-    Checks that resource path is inside request's template_dir.
+        Checks that resource path is inside request's template_dir.
 
-    """
-    template_dir = get_template_dir(request)
-    filename = request.matchdict['template_path']
-    file_path = normpath(abspath(join(template_dir, filename)))
-    if not file_path.startswith(template_dir):
-        NotFound('Ressource not found.')
-    if filename.endswith('/'):  # Preserve trailing '/'
-        file_path += '/'
-    return file_path
+        """
+        template_dir = self.get_template_dir(request)
+        filename = request.matchdict['template_path']
+        file_path = normpath(abspath(join(template_dir, filename)))
+        if not file_path.startswith(template_dir):
+            NotFound('Ressource not found.')
+        if filename.endswith('/'):  # Preserve trailing '/'
+            file_path += '/'
+        return file_path
 
-
-def get_resource(request):
-    """Return the resource matching request.
-
-    Return value is a :py:class:`FileResource` or :py:class`DirResource`.
-
-    """
-    path = get_resource_path(request)
-    engine = Jinja2Engine()
-    filename_engine = FilenameEngine()
-    if isdir(path):
-        resource = resources.DirResource(path=path, engine=engine,
-                                         filename_engine=filename_engine)
-    else:
-        resource = resources.FileResource(path=path, engine=engine,
-                                          filename_engine=filename_engine)
-    return resource
+    def get_template_dir(self, request):
+        """Return validated template directory configuration for request."""
+        try:
+            template_dir = request.registry.settings['diecutter.template_dir']
+        except KeyError:
+            error_msg = 'Missing mandatory "diecutter.template_dir" setting.'
+            raise ConfigurationError(error_msg)
+        if not template_dir:
+            error_msg = 'Mandatory "diecutter.template_dir" setting is empty.'
+            raise ConfigurationError(error_msg)
+        return template_dir
 
 
 def to_boolean(value):
@@ -144,51 +192,11 @@ def to_boolean(value):
     return _BOOL_STATES[value]
 
 
-def is_readonly(request):
-    """Return "readonly" flag status (boolean) for request.
-
-    As an example, PUT operations should be forbidden if readonly flag is On.
-
-    """
-    return to_boolean(request.registry.settings.get('diecutter.readonly',
-                                                    False))
-
-
 def get_accepted_types(request):
     """Return list of accepted content types from request's 'accept' header."""
     if isinstance(request.accept, MIMENilAccept):  # Not explicitely requested.
         return ['*/*']  # Default.
     return [item for item in request.accept]
-
-
-def get_writers(request, resource, context):
-    """Return iterable of writers."""
-    from diecutter.writers import (zip_directory_response, file_response,
-                                   targz_directory_response)
-    if resource.is_file:
-        return [file_response]
-    else:
-        accepted_mime_types = get_accepted_types(request)
-        # Reference.
-        mime_type_map = {'application/zip': [zip_directory_response],
-                         'application/gzip': [targz_directory_response],
-                         }
-        # Aliases.
-        mime_type_map['application/x-gzip'] = mime_type_map['application/gzip']
-        # Fallback.
-        mime_type_map['*/*'] = mime_type_map['application/zip']
-        for accepted_mime_type in accepted_mime_types:
-            try:
-                return mime_type_map[accepted_mime_type]
-            except KeyError:
-                pass
-        raise HTTPNotAcceptable('Supported mime types: %s'
-                                % ', '.join(sorted(mime_type_map.keys())))
-
-
-def get_dispatcher(request, resource, context, writers):
-    """Return simple dispatcher (later, would read configuration)."""
-    return FirstResultDispatcher(writers)
 
 
 class FirstResultDispatcher(object):
